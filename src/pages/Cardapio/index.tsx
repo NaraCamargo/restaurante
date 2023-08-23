4ede9a467e0636511d224995d730119dd64b574f
NaraCamargo <naraliggia2@gmail.com>
2023-08-23 08:58:56 -0300

    23/08

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,12 +1,20 @@
 import styles from './Cardapio.module.scss';
-import Logo from '../../assets/logo.svg';
+import Logo from 'assets/logo.svg';
 
 export default function Cardapio(){
     return(
-        <main>
-            <nav className={styles.menu}>
-                <img src={Logo} alt="Logo do Restaurante" />
-            </nav>
-        </main>
+    <main>
+        <nav className={styles.menu}>
+            <img src={Logo} alt="Logo do Restaurante" />
+        </nav>
+        <header className={styles.header}>
+            <div className={styles.header__text}>
+             A casa do código e da massa!
+            </div>
+        </header>      
+        <section>
+            <h3>Cardapio</h3>
+        </section>
+    </main>
     );
 } 
