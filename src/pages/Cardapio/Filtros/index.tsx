fd3d35f476abb0e284080a5131d3b5dca3ab96b3
NaraCamargo <naraliggia2@gmail.com>
2023-09-13 08:04:44 -0300

    13/09

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -3,7 +3,7 @@ import filtros from './filtros.json'
 
 type IOpcao = typeof filtros[0];
 
-export default function Filtros(opcao: IOpcao) {
+export default function Filtros() {
     return(
         <div className={styles.filtro}>
             {filtros.map((opcao) => (
